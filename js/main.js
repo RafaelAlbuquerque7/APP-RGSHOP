d98881a23a76a721b1484de64535dbca46ad95a0
RafaelAlbuquerque7 <rafaeldeandradealbuquerque7@gmail.com>
2022-11-23 18:28:49 -0300

    Adicionado a parte inicial do Renato

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let url = `https://elated-yak-turtleneck.cyclic.app/produtos`;
 const main = document.querySelector('main');
 
 window.addEventListener('load', e => {
+    loc();
     postComp();
     "use strict";//restrito a funcionar em navegadores comES6 >
     if("serviceWorker" in navigator){
@@ -31,3 +32,34 @@ function createArticle(article){
            </div>
     `
 }
+
+let posicaoInicial;//variavel para capturar a posicao
+const map = document.getElementById('mapa');
+ 
+const sucesso = (posicao) => {//callback de sucesso para captura da posicao
+    posicaoInicial = posicao;
+    map.src = "http://maps.google.com/maps?q="+ posicaoInicial.coords.latitude+"," + posicaoInicial.coords.longitude +"&z=16&output=embed"
+};
+ 
+const erro = (error) => {//callback de error (falha para captura de localizacao)
+    let errorMessage;
+    switch(error.code){
+        case 0:
+            errorMessage = "Erro desconhecido"
+        break;
+        case 1:
+            errorMessage = "Permissão negada!"
+        break;
+        case 2:
+            errorMessage = "Captura de posição indisponível!"
+        break;
+        case 3:
+            errorMessage = "Tempo de solicitação excedido!"
+        break;
+    }
+    console.log('Ocorreu um erro: ' + errorMessage);
+};
+ 
+function loc () {
+    navigator.geolocation.getCurrentPosition(sucesso, erro);
+};
--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
 let cacheName = "APP-RGSHOP";
-let filesToCache = ["/", "/index.html",
+let filesToCache = ["/", "/index.html", "/offline.html",
                 "/css/style.css", "/js/main.js"];
+                
+const OFFLINE_VERSION = 1;
+const OFFLINE_URL = "offline.html"
 
 /* inicializando a service worker e fazendo o 
 download do conteúdo da aplicação */
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+      caches.open(cacheName).then(function (cache) {
+        return cache.addAll(filesToCache);
+      })
+     );
 });
+
 
 /* disponibilizando o conteudo quando estiver offline */
 self.addEventListener("fetch", (e) => {
@@ -20,4 +24,31 @@ self.addEventListener("fetch", (e) => {
     })
   );
 });
+
+// if (event.request.mode === "navigate") {
+//   event.respondWith(
+//     (async () => {
+//       try {
+//         // First, try to use the navigation preload response if it's supported.
+//         const preloadResponse = await event.preloadResponse;
+//         if (preloadResponse) {
+//           return preloadResponse;
+//         }
+
+//         // Always try the network first.
+//         const networkResponse = await fetch(event.request);
+//         return networkResponse;
+//       } catch (error) {
+//         // catch is only triggered if an exception is thrown, which is likely
+//         // due to a network error.
+//         // If fetch() returns a valid HTTP response with a response code in
+//         // the 4xx or 5xx range, the catch() will NOT be called.
+//         console.log("Fetch failed; returning offline page instead.", error);
+
+//         const cache = await caches.open(CACHE_NAME);
+//         const cachedResponse = await cache.match(OFFLINE_URL);
+//         return cachedResponse;
+//       }
+//     })()
+//   );
 
