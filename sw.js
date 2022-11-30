88617f9a4c631e77507578710eedb3d470866e24
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-11-30 13:57:42 -0300

    Teste do geolocation 1

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ let filesToCache = ["/", "favicon.ico","/index.html", "/fallback/offline.html",
 
 /* inicializando a service worker e fazendo o 
 download do conteúdo da aplicação */
+
 self.addEventListener("install", (e) => {
   e.waitUntil(
       caches.open(cacheName).then(function (cache) {
@@ -12,6 +13,7 @@ self.addEventListener("install", (e) => {
       })
      );
 });
+
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
@@ -21,49 +23,32 @@ self.addEventListener('activate', (e) => {
   )
 })
 
-
-// /* disponibilizando o conteudo quando estiver offline */
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     caches.match(e.request).then((response) => {
-//       return response || fetch(e.request).then(fetchRes => {
-//         return caches.open(cacheArmazena).then(cache =>{
-//           console.log(cacheArmazena)
-//          cache.put(e.request.url, fetchRes.clone())
-//          return fetchRes;
-//         })
-//       })
-//     }).catch(() => {return caches.match("/fallback/offline.html");})     
-//   );
-// });
-
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+        return response || fetch(e.request);
+    }).catch(() => {
+      console.log("Fetch failed; returning offline page instead.");
+      const cachedResponse = caches.open(cacheName).match('/fallback/offline.html');
+    //  const cachedResponse = caches.match('/fallback/offline.html');
+      return cachedResponse;
+  })
   );
 });
 
 
 // self.addEventListener("fetch", (e) => {
-//     e.respondWith(
-//       (async () => {
+//     e.respondWith(()=>{
 //         try {
-//           const resposta = await e.resposta;
-//           console.log(resposta);
-//           if (resposta) {
-//             return resposta;
-//           }
-//           const networkResponse = await fetch(e.request);
-//           return networkResponse;
+//         caches.match(e.request).then((response) => {
+//            return response || fetch(e.request);
+//         })
 //         } catch (error) {
 //           console.log("Fetch failed; returning offline page instead.", error);
-//           const cache = await caches.open(cacheName);
-//           const cachedResponse = await cache.match('/fallback/offline.html');
+//           const cache = caches.open(cacheName);
+//           const cachedResponse = cache.match('/fallback/offline.html');
 //           return cachedResponse;
-//         }
-//       })
-//     );
-  
+//         }     
+//       });
 // });
+
