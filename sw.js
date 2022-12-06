dcb246c95598335b5b6de4b6a45558c6e31d1871
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2022-12-05 23:44:58 -0300

    Retirado os comentarios da sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = "APP-RGSHOP";
-// Customize this with a different URL if needed.
 const OFFLINE_URL = "/fallback";
 
 self.addEventListener("install", (event) => {
@@ -13,13 +12,10 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-    // Tell the active service worker to take control of the page immediately.
     self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-    // We only want to call event.respondWith() if this is a navigation request
-    // for an HTML page.
     if (event.request.mode === "navigate") {
         if (event.request.url.match(/SignOut/)) {
             return false;
